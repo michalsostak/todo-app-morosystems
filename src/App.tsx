43dd3361f3a5bdc2cf27e410ea7e80b655beba90
Michal Sostak <michal.sostak@gmail.com>
2023-10-28 20:07:39 +0200

    feat: theme toggle

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { FilterTodos } from "./components/FilterTodos";
 import { Header } from "./components/Header";
 import { LayoutWrapper } from "./components/LayoutWrapper";
 import { ListTodos } from "./components/ListTodos";
+import { ThemeToggle } from "./components/ThemeToggle";
 import { TodosCount } from "./components/TodosCount";
 
 const App = () => {
@@ -13,6 +14,7 @@ const App = () => {
             <ListTodos />
             <FilterTodos />
             <TodosCount />
+            <ThemeToggle />
         </LayoutWrapper> 
     );
 };
